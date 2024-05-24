[svgd] Fixed the gradient step for SVGD class. Compared it with original implementation from the paper.

svgd/density.py
@@ -24,8 +24,46 @@ class Density(eqx.Module):
         score_fun = jax.grad(log_density, argnums=0)
         return jax.vmap(score_fun)(x)
 
-# Define a Gaussian PDF for testing
+# Define a unimodal gaussian pdf
 def gaussian_pdf(x, params):
     mean = params['mean']
     cov = params['covariance']
     return multivariate_normal.pdf(x, mean, cov)
+
+
+# define a bimodal gaussian
+def gaussian_mixture_pdf(x, params):
+    """
+    Compute the probability density of a Gaussian Mixture model.
+
+
+    Parameters:
+        x: jnp.ndarray
+            The input data points of shape (num_points, num_features)
+        weights: jnp.ndarray
+            The mixture weights os shape (num_components,).
+        means: jnp.ndarray
+            The means of shape (num_components, num_features).
+        covs : jnp.ndarray
+            The covariances of shape (num_components, num_features,
+            num_features).
+
+    Returns:
+    jnp.ndarray
+        The probability densities for each data point, shape (num_points,)
+    """
+
+    # params
+    mean = params['mean']
+    cov = params['covariance']
+    weights = params['weights']
+    num_components = weights.shape[0]
+
+    # Compute the PDF of each component
+    component_pdfs = jnp.array([multivariate_normal.pdf(x, mean[k], cov[k])
+                                for k in range(num_components)])
+
+    # Weighted sum of the component PDFs
+    pdf = jnp.dot(weights, component_pdfs)
+
+    return pdf

svgd/kernel.py
@@ -2,6 +2,7 @@ import jax
 import jax.numpy as jnp
 import equinox as eqx
 
+
 class Kernel(eqx.Module):
     """ Base Kernel class.
     """
@@ -18,7 +19,7 @@ class Kernel(eqx.Module):
         self.params = params
         self.kernel_fun = kernel_fun
 
-    def __call__(self, x, y):
+    def __call__(self, x, y, params=None):
         """
         Compute the gram matrix in blocks of 5000 for memory efficiency.
         :param x: Input 1
@@ -28,10 +29,14 @@ class Kernel(eqx.Module):
         :return: Gram matrix between x and y with the specified kernel
         :rtype: array
         """
-        K = jax.vmap(lambda x1: jax.vmap(lambda y1: self.kernel_fun(x1, y1, self.params))(y))(x)
+        if params is None:
+            params = self.params
+        K = jax.vmap(
+            lambda x1: jax.vmap(lambda y1: self.kernel_fun(x1, y1, params))(
+                y))(x)
         return K
-    
-    def gradient_wrt_first_arg(self, x, y):
+
+    def gradient_wrt_first_arg(self, x, y, params=None):
         """
         Compute the gradient of the kernel with respect to the first argument.
         :param x: Input 1
@@ -41,9 +46,14 @@ class Kernel(eqx.Module):
         :return: Gradient of the kernel with respect to the first argument
         :rtype: array
         """
+        if params is None:
+            params = self.params
         grad_kernel_fun = jax.grad(self.kernel_fun, argnums=0)
-        grad_K = jax.vmap(lambda x1: jax.vmap(lambda y1: grad_kernel_fun(x1, y1, self.params))(y))(x)
+        grad_K = jax.vmap(
+            lambda x1: jax.vmap(lambda y1: grad_kernel_fun(x1, y1, params))(
+                y))(x)
         return grad_K
+
 
 def sqeuclidean_distances(x, y):
     """
@@ -58,10 +68,10 @@ def sqeuclidean_distances(x, y):
     """
     return jnp.sum((x - y) ** 2)
 
+
+# Example kernel function
 def rbf_kernel(x, y, params):
+    # print(params['length_scale'])
     length_scale = params['length_scale']
-    if isinstance(length_scale, float):
-        length_scale = [length_scale]
-    dist = sqeuclidean_distances(x, y)
-    kvals = jnp.array([jnp.exp(-dist / (2 * l ** 2)) for l in length_scale])
-    return jnp.sum(kvals)
+    sqdist = jnp.sum((x - y) ** 2)
+    return jnp.exp(-sqdist / (2 * length_scale ** 2))

svgd/models.py
@@ -4,6 +4,7 @@ import numpy as np
 import jax
 from tqdm import tqdm
 import equinox as eqx
+import optax
 
 
 class SVGDModel(eqx.Module):
@@ -12,40 +13,61 @@ class SVGDModel(eqx.Module):
     def __init__(self, kernel):
         self.kernel_obj = kernel
 
-    @eqx.filter_jit
-    def calculate_gradient(self, density, particles):
+    # @eqx.filter_jit
+    def calculate_gradient(self, density, particles, kernel_params=None):
         num_particles = particles.shape[0]
-        gram_matrix = self.kernel_obj(particles, particles)
+        gram_matrix = self.kernel_obj(particles, particles, kernel_params)
         score_matrix = density.score(particles)
         kernel_gradient = self.kernel_obj.gradient_wrt_first_arg(particles,
-                                                                 particles)
+                                                                 particles,
+                                                                 kernel_params)
 
         # calculate the terms
         kernel_score_term = jnp.einsum('ij,jk->ik', gram_matrix, score_matrix)
-        kernel_gradient_term = jnp.sum(kernel_gradient, axis=1)
+        kernel_gradient_term = jnp.sum(kernel_gradient, axis=0)
         phi = (kernel_score_term + kernel_gradient_term) / num_particles
-
         return phi
 
-    @eqx.filter_jit
-    def update(self, particles, density, step_size):
-        gradient = self.calculate_gradient(density, particles)
-        updated_particles = particles + step_size * gradient
-        return updated_particles
+    def calculate_length_scale(self, particles):
+        pairwise_distances = jnp.sqrt(jnp.sum((particles[:, None, :] -
+                                               particles[None, :, :]) ** 2,
+                                              axis=-1))
+        median_distance = jnp.median(pairwise_distances)
+        new_length_scale = 0.5 * median_distance / jnp.log(len(particles))
+        return new_length_scale
 
-    def predict(self, particles, density, step_size, num_iterations,
-                trajectory=False):
+    # @eqx.filter_jit
+    def update(self, particles, density, optimizer, optimizer_state,
+               kernel_params=None):
+        gradient = self.calculate_gradient(density, particles, kernel_params)
+        print(f'Gradient: {gradient}')
+        updates, optimizer_state = optimizer.update(gradient, optimizer_state,
+                                              particles)
+        updated_particles = optax.apply_updates(particles, updates)
+        print(f'Updated Position: {updated_particles}')
+        return updated_particles, optimizer_state
+
+    def predict(self, particles, density, num_iterations, optimizer,
+                trajectory=False, adapt_length_scale=False):
         particle_trajectory = np.zeros((num_iterations + 1, particles.shape[0],
                               particles.shape[1]))
         start = deepcopy(particles)
         particle_trajectory[0] = start
+        kernel_params = self.kernel_obj.params # initialize
 
-        # append
+        # initialize the optimizer
+        optimizer_state = optimizer.init(start)
+
         for i in tqdm(range(1, num_iterations + 1)):
-            updated_particles = self.update(start, density, step_size)
-            # print(updated_particles)
-            particle_trajectory[i] = updated_particles
-            start = updated_particles
+            if 'length_scale' in kernel_params and adapt_length_scale:
+                # Update length scale outside of JIT
+                kernel_params['length_scale'] = self.calculate_length_scale(start)
+            print(f'Kernel params: {kernel_params}')
+            start, optimizer_state = self.update(start, density,
+                                                             optimizer,
+                                                             optimizer_state,
+                                                             kernel_params)
+            particle_trajectory[i] = start
 
         if trajectory:
             return particle_trajectory[-1], particle_trajectory

svgd/plots.py
@@ -1,21 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+import jax.numpy as jnp
 
 def plot_distributions(initial_particles, transported_particles, density_params):
     plt.figure(figsize=(10, 6))
 
     # Plot histogram of initial particles
-    plt.hist(initial_particles, bins=30, density=True, alpha=0.4, color='b', label='Initial Particles')
+    plt.hist(initial_particles, bins=30, density=True, alpha=0.4, color='b',
+             histtype='bar', label='Initial Particles')
 
     # Plot histogram of transported particles
-    plt.hist(transported_particles, bins=30, density=True, alpha=0.4, color='g', label='Transported Particles')
+    plt.hist(transported_particles, bins=30, density=True, alpha=0.4,
+             color='g', histtype='bar', label='Transported Particles')
 
     # Plot the target density function
     mean = density_params['mean'][0]
     std_dev = np.sqrt(density_params['covariance'][0, 0])
     x = np.linspace(mean - 4 * std_dev, mean + 4 * std_dev, 1000)
     y = norm.pdf(x, mean, std_dev)
+    plt.plot(x, y, 'r-', lw=2, label='Target Distribution')
+
+    plt.title('Initial and Final Distributions of Particles')
+    plt.xlabel('Particle Value')
+    plt.ylabel('Density')
+    plt.legend()
+    plt.show()
+
+
+# Plotting function
+def plot_gaussian_mixture_distribution(initial_particles,
+                                       transported_particles, density):
+    plt.figure(figsize=(10, 6))
+
+    plt.hist(initial_particles, bins=30, density=True, alpha=0.4, color='b', label='Initial Particles')
+    plt.hist(transported_particles, bins=30, density=True, alpha=0.4, color='g', label='Transported Particles')
+
+
+    # plot density object
+    means = density.params['mean']
+    cov = density.params['mean']
+    weights = density.params['weights']
+    expected_mean = jnp.sum(weights * means)
+    expected_var = (jnp.sum(weights * (cov.squeeze() + means ** 2))
+                    - expected_mean ** 2)
+    expected_std = jnp.sqrt(expected_var)
+
+
+    # Plot the target density function
+    x = np.linspace(expected_mean - 4 * expected_std,
+                    expected_mean + 4 * expected_std, 1000)
+    y = density(x)
     plt.plot(x, y, 'r-', lw=2, label='Target Distribution')
 
     plt.title('Initial and Final Distributions of Particles')

tests/test_models.py
@@ -1,24 +1,26 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+
 import unittest
 import jax.numpy as jnp
 import jax.random as jrandom
 import numpy as np
 from svgd import kernel, density, models, plots
+import optax
 
 
 class TestModels(unittest.TestCase):
-    def setUp(self):
-        return None
 
     def test_1d_gaussian_test_shift_mean(self):
         # define model
-        model_params = {'length_scale': 20.0}
+        model_params = {'length_scale': 50.0}
         model_kernel = kernel.Kernel(kernel.rbf_kernel, model_params)
         transporter = models.SVGDModel(kernel=model_kernel)
 
 
         # define particles
         key = jrandom.PRNGKey(10)
-        particles = jrandom.normal(key=key, shape=(500, 1))
+        particles = jrandom.normal(key=key, shape=(1000, 1))
 
 
         # define 1D density
@@ -27,7 +29,7 @@ class TestModels(unittest.TestCase):
         density_obj = density.Density(density.gaussian_pdf, density_params)
 
         # transport model
-        num_iterations, step_size = 1000, 1e-2
+        num_iterations, step_size = 5000, 1e-2
         transported, trajectory = transporter.predict(particles, density_obj,
                                                step_size, num_iterations,
                                                trajectory=True)
@@ -46,10 +48,10 @@ class TestModels(unittest.TestCase):
         plots.plot_distributions(particles, trajectory[-1], density_params)
 
         # assert
-        np.testing.assert_almost_equal(expected_mean, observed_mean, decimal=1,
-                 err_msg='Means do not match.')
-        np.testing.assert_almost_equal(expected_var, observed_var, decimal=1,
-                 err_msg='Variances do not match.')
+        np.testing.assert_array_almost_equal([expected_mean, expected_var],
+                                             [observed_mean, observed_var],
+                                             decimal=1,
+                                             err_msg='Metrics do not match.')
 
     def test_1d_gaussian_dilate_variance(self):
         # define model
@@ -60,16 +62,17 @@ class TestModels(unittest.TestCase):
 
         # define particles
         key = jrandom.PRNGKey(10)
-        particles = jrandom.normal(key=key, shape=(500, 1))
+        particles = jrandom.normal(key=key, shape=(1000, 1))
 
 
         # define 1D density
         density_params = {'mean': jnp.array([10.0]),
             'covariance': jnp.array([[0.1]])}
-        density_obj = density.Density(density.gaussian_pdf, density_params)
+        density_obj = density.Density(density.gaussian_pdf,
+                                      density_params)
 
         # transport model
-        num_iterations, step_size = 1000, 1e-2
+        num_iterations, step_size = 5000, 1e-2
         transported, trajectory = transporter.predict(particles, density_obj,
                                                step_size, num_iterations,
                                                trajectory=True)
@@ -88,10 +91,64 @@ class TestModels(unittest.TestCase):
         plots.plot_distributions(particles, trajectory[-1], density_params)
 
         # assert
-        np.testing.assert_almost_equal(expected_mean, observed_mean, decimal=1,
-                 err_msg='Means do not match.')
-        np.testing.assert_almost_equal(expected_var, observed_var, decimal=1,
-                 err_msg='Variances do not match.')
+        np.testing.assert_array_almost_equal([expected_mean, expected_var],
+                                             [observed_mean, observed_var],
+                                             decimal=1,
+                                             err_msg='Metrics do not match.')
+
+
+    def test_1d_gaussian_mixture(self):
+        # define model
+        model_params = {'length_scale': 0.3}
+        model_kernel = kernel.Kernel(kernel.rbf_kernel, model_params)
+        transporter = models.SVGDModel(kernel=model_kernel)
+
+
+        # define particles
+        key = jrandom.PRNGKey(10)
+        particles = jrandom.normal(key=key, shape=(1000, 1)) - 10
+
+
+        # define 1D mixture density
+        means = jnp.array([[-2.0], [2.0]])
+        covariances = jnp.array([[[1]], [[1]]])
+        weights = jnp.array([1/3, 2/3])
+        density_params = {'mean': means, 'covariance': covariances,
+                          'weights': weights}
+        density_obj = density.Density(density.gaussian_mixture_pdf,
+                                      density_params)
+
+
+        # transport model
+        num_iterations, step_size = 100, 1e-1
+
+        # define the optimizer
+        optimizer = optax.adam(learning_rate=step_size)
+        transported, trajectory = transporter.predict(particles, density_obj,
+                                               num_iterations, optimizer,
+                                                      trajectory=True,
+                                                      adapt_length_scale=False)
+
+        # check mean
+        expected_mean = jnp.sum(weights * means)
+        expected_var = (jnp.sum(weights * (covariances.squeeze() + means**2))
+                        - expected_mean**2)
+        observed_mean = jnp.mean(transported[-1])
+        observed_var = jnp.var(transported[-1])
+
+        print(f'Means: Expected={expected_mean} Observed={observed_mean}')
+        print(f'Variance: Expected={expected_var} Observed={observed_var}')
+
+        # # plot
+        plots.plot_gaussian_mixture_distribution(particles, trajectory[-1],
+                                                 density_obj)
+        #
+        # # assert
+        # np.testing.assert_array_almost_equal([expected_mean, expected_var],
+        #                                      [observed_mean, observed_var],
+        #                                      decimal=1,
+        #                                      err_msg='Metrics do not match.')
+
 
 
 