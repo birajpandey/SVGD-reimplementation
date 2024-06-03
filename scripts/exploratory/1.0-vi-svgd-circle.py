c7ab0c8074f2666885664101aa3c71f6c67081cd
Vas <vasilin97@gmail.com>
2024-06-03 16:27:46 -0700

    Make pretty pictures with a circle distribution.

--- scripts/exploratory/1.0-vi-svgd-circle.py
+++ scripts/exploratory/1.0-vi-svgd-circle.py
@@ -84,33 +84,37 @@ model_kernel = kernel.Kernel(kernel.rbf_kernel, model_params)
 transporter = models.SVGDModel(kernel=model_kernel)
 
 # transport
-num_iterations, step_size = 40000, 2
+num_iterations, step_size = 20000, 2
 transported, trajectory = transporter.predict(
     particles, distribution.score, num_iterations, step_size, trajectory=True
 )
 
 # plot density
 grid_res = 100
-# Input locations at which to compute log-probabilities
-x_plot = np.linspace(-5, 5, grid_res)
+# Input locations at which to compute probabilities
+x_plot = np.linspace(-4.5, 4.5, grid_res)
 x_plot = np.stack(np.meshgrid(x_plot, x_plot), axis=-1)
 
 # Plot density
 prob = distribution.pdf(x_plot.reshape(-1, 2)).reshape(grid_res, grid_res)
 plt.figure(figsize=(5, 5))
-plt.contourf(x_plot[:, :, 0], x_plot[:, :, 1], prob, cmap="magma")
+plt.contourf(x_plot[:, :, 0], x_plot[:, :, 1], jnp.cbrt(jnp.cbrt(prob)), cmap="magma")
 
 # plot initial particles
-plt.scatter(particles[:, 0], particles[:, 1], zorder=2, c="w", s=10, label="initial", alpha=0.3)
+plt.scatter(particles[:, 0], particles[:, 1], zorder=2, c="w", s=10, label="initial sample", alpha=0.5)
 
 # plot final particles
-plt.scatter(transported[:, 0], transported[:, 1], zorder=2, c='r', s=10, label="final", alpha=0.8)
+plt.scatter(transported[:, 0], transported[:, 1], zorder=2, c='r', s=10, label="final sample", alpha=0.6)
 plt.legend()
+plt.title("Transported Particles")
 
 plt.show()
 
 # plot trajectory
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111)
-ax = plots.plot_2d_trajectories(ax, trajectory, 1000, seed=20, alpha=0.03)
+ax = plots.plot_2d_trajectories(ax, trajectory, 2000, seed=20, alpha=0.03)
+ax.set_xlim(-2, 4)
+ax.set_ylim(-3, 3)
+ax.set_title("Trajectories of 2000 particles")
 plt.show()
